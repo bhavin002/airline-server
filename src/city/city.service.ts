68be34fc0286f9be3acab1a4a812daf2c9d7988b
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2024-01-04 15:52:03 +0530

    replace the source and destination with city

---
+++ src/city/city.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CityDto } from './dto/city.dto';
+import { City } from './entity/city.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class CityService {
+  constructor(
+    @InjectRepository(City)
+    private readonly destinationRepository: Repository<City>,
+  ) {}
+
+  async addCity(addDestination: CityDto): Promise<CityDto> {
+    const City: CityDto = new CityDto();
+    City.cityName = addDestination.cityName;
+    return this.destinationRepository.save(City);
+  }
+}
--- src/city/dto/city.dto.ts
+++ src/city/dto/city.dto.ts
@@ -1,10 +1,10 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
-export class DestinationDto {
+export class CityDto {
   @IsOptional()
   @IsNotEmpty()
   id: string;
 
   @IsNotEmpty()
-  destinationName: string;
+  cityName: string;
 }
